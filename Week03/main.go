e93da3a6811a4b2727fbde4d03671c3ac85a399d
baltarsa <lhy4github.com>
2022-04-04 11:01:12 +0800

    题意理解有问题，重新提交

--- Week03/main.go
+++ Week03/main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"sync"
+	"sync/atomic"
 	"syscall"
 	"time"
 
@@ -15,89 +15,152 @@ import (
 )
 
 type Server struct {
-	server   http.Server
-	mux      *http.ServeMux
+	http.Server
+	Name    string
+	running int32
+}
+
+func (srv *Server) HandleFunc(pattern string, handlerFunc http.HandlerFunc) {
+	if srv.Server.Handler == nil {
+		srv.Server.Handler = http.NewServeMux()
+	}
+	mux := srv.Server.Handler.(*http.ServeMux)
+	mux.HandleFunc(pattern, handlerFunc)
+}
+
+func (srv *Server) Shutdown() error {
+	var err error
+	if atomic.SwapInt32(&srv.running, 0) == 1 {
+		err = srv.Server.Shutdown(context.TODO())
+	}
+	return err
+}
+
+func (srv *Server) ListenAndServe() error {
+	if atomic.SwapInt32(&srv.running, 1) == 1 {
+		return nil
+	}
+	defer func() { atomic.SwapInt32(&srv.running, 0) }()
+
+	return srv.Server.ListenAndServe()
+}
+
+type ServerGroup struct {
+	servers  []*Server
 	errgroup errgroup.Group
 	sigch    chan os.Signal
 	err      error
-	mu       sync.Mutex
+	running  int32
 }
 
-func (serv *Server) Start(addr string, h http.Handler, sigset []os.Signal) error {
-	serv.sigch = make(chan os.Signal, 1)
+func (sg *ServerGroup) ListenAndServe(servers []*Server, sigset []os.Signal) error {
+	if len(servers) <= 0 {
+		return errors.Errorf("Empty server list.")
+	}
+
+	if atomic.SwapInt32(&sg.running, 0) == 1 {
+		return errors.Errorf("Server Group is running.")
+	}
+
+	sg.sigch = make(chan os.Signal)
 	if len(sigset) > 0 {
-		signal.Notify(serv.sigch, sigset...)
+		signal.Notify(sg.sigch, sigset...)
 	}
 
-	handler := h
-	if handler == nil {
-		handler = serv.mux
-	}
-	serv.server = http.Server{Addr: addr, Handler: handler}
+	sg.servers = []*Server{}
+	atomic.StoreInt32(&sg.running, 1)
+	for _, server := range servers {
+		server := server
+		sg.servers = append(sg.servers, server)
 
-	serv.errgroup.Go(func() error {
-		fmt.Println("Server start...")
-		err := serv.server.ListenAndServe()
-		if err == http.ErrServerClosed && serv.err != nil {
-			return serv.err
+		sg.errgroup.Go(func() error {
+			fmt.Printf("Server %s start...\n", server.Name)
+			err := server.ListenAndServe()
+			err = errors.Wrapf(err, "Server %s stopped", server.Name)
+			if atomic.LoadInt32(&sg.running) == 1 {
+				sg.err = err
+				sg.Stop()
+			}
+			fmt.Printf("Server %s stop...\n", server.Name)
+			return err
+		})
+	}
+
+	sg.errgroup.Go(func() error {
+		var err error
+		sigrecv := <-sg.sigch
+		if sigrecv != nil {
+			err = errors.Errorf("Receive Signal %s\n", sigrecv.String())
+			sg.err = err
 		}
-		serv.err = err
+		sg.Stop()
 		return err
 	})
 
-	serv.errgroup.Go(func() error {
-		sig := <-serv.sigch
-		if sig != nil {
-			//because Stop()->Shutdown() will set errgroup.err first, so here save the sig error
-			serv.err = errors.Errorf("Receive Signal %s\n", sig.String())
-			serv.Stop()
-		}
+	sg.errgroup.Wait()
+	return sg.err
+
+}
+
+func (sg *ServerGroup) Stop() error {
+	if atomic.SwapInt32(&sg.running, 0) == 0 {
 		return nil
-	})
-
-	return serv.errgroup.Wait()
-
-}
-
-func (serv *Server) Stop() error {
-	close(serv.sigch)
-	return serv.server.Shutdown(context.TODO()) //nil
-
-}
-
-func (serv *Server) HandleFunc(pattern string, handlerFunc http.HandlerFunc) {
-	if serv.mux == nil {
-		serv.mu.Lock()
-		if serv.mux == nil {
-			serv.mux = http.NewServeMux()
-		}
-		serv.mu.Unlock()
 	}
-	serv.mux.HandleFunc(pattern, handlerFunc)
+	close(sg.sigch)
+	sg.sigch = nil
+	for _, server := range sg.servers {
+		server.Shutdown()
+	}
+
+	return nil
+
 }
 
-func (serv *Server) Error() error {
+func (serv *ServerGroup) Sig(sig os.Signal) {
+	serv.sigch <- sig
+}
+
+func (serv *ServerGroup) Error() error {
 	return serv.err
 }
 
 func main() {
-	serv := &Server{}
+	var servers []*Server
+	var abortServer *Server
+
+	basePort := 8080
+	for num := 0; num < 10; num++ {
+		num := num
+		srv := &Server{}
+		srv.Name = fmt.Sprintf("Serv%03d", num)
+		srv.Server.Addr = fmt.Sprintf("localhost:%d", basePort+num)
+		srv.HandleFunc(
+			"/index",
+			func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "Hello, This is Server%03d!\n", num)
+			},
+		)
+		servers = append(servers, srv)
+		if num == 1 {
+			abortServer = srv
+			_ = abortServer
+		}
+	}
+
+	sg := &ServerGroup{}
 	sigs := []os.Signal{
 		syscall.SIGINT,
 		syscall.SIGTERM,
 	}
 
-	serv.HandleFunc("/index",
-		func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintln(w, "Hello World")
-		})
-
-	go func() { //killed because of timeout or signal
+	go func() {
 		time.Sleep(10 * time.Second)
-		serv.Stop()
+		//sg.Stop()
+		//abortServer.Shutdown()
+		sg.Sig(syscall.SIGINT)
 	}()
 
-	err := serv.Start("localhost:8080", nil, sigs)
+	err := sg.ListenAndServe(servers, sigs)
 	if err != nil {
 		fmt.Printf("Error happened when server running: %v\n", err)
 	}
